e3eba6ea875fcc4630d84dc5b83edcd70c7abf81
theoneandnoely <nmarkham96@gmail.com>
2024-06-06 19:35:41 +0100

    added update_properties function

--- DaftScraper/scrape_daft.py
+++ DaftScraper/scrape_daft.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from os import getenv
 from os.path import exists
 import json
+import sqlite3
 
 def get_coords(address:str) -> List[float]:
     '''
@@ -40,7 +41,7 @@ def get_coords(address:str) -> List[float]:
 
 def scrape_daft(campaign_id, num_beds, max_price):
 
-    url = f'https://www.daft.ie/property-for-rent/dublin-city?rentalPrice_to={max_price}&numBeds_from={num_beds}&sort=publishDateDesc'
+    url = f'https://www.daft.ie/property-for-rent/dublin-city?rentalPrice_to={str(max_price)}&numBeds_from={str(num_beds)}&sort=publishDateDesc'
     with requests.get(url) as page:
         soup = BeautifulSoup(page.text, "html.parser")
     
@@ -67,6 +68,7 @@ def scrape_daft(campaign_id, num_beds, max_price):
                     'link':f'https://www.daft.ie{link}', 
                     'address':address, 
                     'img_url':img_url, 
+                    'num_beds':num_beds,
                     'price':price, 
                     'latitude':lat, 
                     'longitude':lon
@@ -74,3 +76,40 @@ def scrape_daft(campaign_id, num_beds, max_price):
             )
             counter += 1
     return properties
+
+def update_properties(daft_id:str, link:str, address:str, img_url:str, num_beds:int, price:str, lat:float, lon:float) -> None:
+    '''
+    Add record to the db for a single property.
+
+    INPUT:
+    - daft_id (str): The id for the property on daft
+    - link (str): The url to the ad on daft
+    - address (str): The address as shown in the ad on daft
+    - img_url (str): The url for the thumbnail shown in the ad on daft
+    - price (str): the '€xxx per month' or '€xxx per week' price noted on the ad on daft
+    - lat (float): the latitude of the property pulled from position stack
+    - lon (float): the longitude of the property pulled from position stack
+
+    OUTPUT:
+    None
+    '''
+    # Convert Price from string to float, normalised to a monthly price
+    if 'week' in price:
+        price_flt = (float(price.split('€')[1].split(' ')[0].replace(',',''))*52)/12
+    else:
+        price_flt = float(price.split('€')[1].split(' ')[0].replace(',',''))
+
+    # Raise error if db doesn't exist, otherwise connect to db
+    if exists('daft_data.db') == False:
+        raise RuntimeError('Database not initialised')
+    
+    conn = sqlite3.connect('daft_data.db')
+    curs = conn.cursor()
+
+    values = f'("{daft_id}", "{link}", "{address}", "{img_url}", {num_beds}, {price_flt}, {lat}, {lon})'
+
+    curs.execute(f'INSERT INTO properties (daft_id, link, address, img_url, num_beds, price, latitude, longitude) VALUES {values};')
+    conn.commit()
+
+    curs.close()
+    conn.close()
